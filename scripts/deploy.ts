8eaf363f8130e928a09e101655d703b47ce28c4a
0xukhezo <ukhezo.web3@gmail.com>
2023-04-14 13:03:30 +0200

    feat:change contracts to dont need the nonce

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,13 +3,15 @@ import { developmentChains } from "../helper-hardhat-config";
 import { verify } from "../utils/verify";
 
 async function main() {
+
   const CampaignsFactory = await ethers.getContractFactory(
     "CampaignFactory"
   );
   const _tokenX = "0x42bb40bF79730451B11f6De1CbA222F17b87Afd7"
   const _owner = "0xa8EC796eE75B04af1223445c587588181CEb56CD"
-  
-  const campaignsFactory = await CampaignsFactory.deploy(_tokenX, _owner);
+  const _usdcAddr = "0xbe49ac1EadAc65dccf204D4Df81d650B50122aB2";
+
+  const campaignsFactory = await CampaignsFactory.deploy(_tokenX, _owner, _usdcAddr);
 
   await campaignsFactory.deployed();
 
@@ -22,7 +24,7 @@ async function main() {
     process.env.POLYGONSCAN_API
   ) {
     console.log("Verifying...");
-    await verify(campaignsFactory.address, [_tokenX, _owner]);
+    await verify(campaignsFactory.address, [_tokenX, _owner, _usdcAddr]);
   }
 }
 
