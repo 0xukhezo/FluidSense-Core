71344bc2781b230c4f23c1e0a4d1731be38dde7a
0xukhezo <ukhezo.web3@gmail.com>
2023-04-27 10:10:19 +0200

    feat:Add withdraw function to campaign contract

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -1,7 +1,6 @@
 //SPDX-License-Identifier: Unlicensed
 pragma solidity 0.8.14;
 
-import "hardhat/console.sol";
 import {SuperTokenV1Library} from "@superfluid-finance/ethereum-contracts/contracts/apps/SuperTokenV1Library.sol";
 import {ISuperToken} from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
@@ -11,9 +10,11 @@ contract Campaign {
 
     ISuperToken internal immutable tokenX;
     ERC20 internal immutable baseToken;
+    address payable owner;
 
     constructor(ISuperToken _tokenX, address _operator) {
         tokenX = _tokenX;
+        owner = payable(_operator);
         tokenX.setMaxFlowPermissions(_operator);
         baseToken = ERC20(tokenX.getUnderlyingToken());
         baseToken.approve(address(tokenX), 2 ** 256 - 1);
@@ -26,5 +27,14 @@ contract Campaign {
             (baseToken.balanceOf(address(this)) * 300) / 10000
         );
         tokenX.upgrade(baseToken.balanceOf(address(this)) * 10 ** 12);
+    }
+
+    function withdraw(uint256 value) public {
+        require(
+            msg.sender == owner,
+            "Only the contract owner can call this function."
+        );
+        uint256 fee = (value * 300) / 10000;
+        tokenX.transfer(owner, fee);
     }
 }
